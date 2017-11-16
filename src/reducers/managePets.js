a1d454996f4135c7575c26a147ddb5d1678c2fb5
colesayer <colesayershapiro@gmail.com>
2017-11-16 15:46:01 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,33 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch(action.type)
+  case 'ADD_PET':
+    return {...state, pets: [...state.pets, action.pet]}
+  case 'REMOVE_PET':
+    let newPets = state.pets.filter((pet) => pet.id !== action.id)
+    return {...state, pets: newPets}
+  default:
+  return state
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
+  const container = document.getElementById('container')
+  const petList = document.createElement('ul')
+  container.appendChild(petList)
+
+  for(let i = 0; i < state.pets.length; i ++){
+    let listItem = document.createElement('li')
+    listItem.innerHTML = state.pets[i].name
+    petList.appenChild(listItem)
+  }
 
 }
+
+// dispatch({type: '@@INIT'})
