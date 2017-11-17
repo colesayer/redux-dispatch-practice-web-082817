28abab85b0a5056fc7aeaef791a1489062a35524
colesayer <colesayershapiro@gmail.com>
2017-11-17 17:01:58 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -2,14 +2,15 @@ export let state;
 
 
 export function managePets(state = {pets: []}, action){
-  switch(action.type)
-  case 'ADD_PET':
-    return {...state, pets: [...state.pets, action.pet]}
-  case 'REMOVE_PET':
-    let newPets = state.pets.filter((pet) => pet.id !== action.id)
-    return {...state, pets: newPets}
-  default:
-  return state
+  switch(action.type){
+    case 'ADD_PET':
+      return {...state, pets: [...state.pets, action.pet]}
+    case 'REMOVE_PET':
+      let newPets = state.pets.filter((pet) => pet.id !== action.id)
+      return {...state, pets: newPets}
+    default:
+    return state
+  }
 }
 
 export function dispatch(action){
@@ -19,14 +20,8 @@ export function dispatch(action){
 
 export function render(){
   const container = document.getElementById('container')
-  const petList = document.createElement('ul')
-  container.appendChild(petList)
-
-  for(let i = 0; i < state.pets.length; i ++){
-    let listItem = document.createElement('li')
-    listItem.innerHTML = state.pets[i].name
-    petList.appenChild(listItem)
-  }
+  let petItems = state.pets.map((pet) => `<li>${pet.name}</li>`).join('')
+  container.innerHTML = `<ul>${petItems}</ul>`
 
 }
 
